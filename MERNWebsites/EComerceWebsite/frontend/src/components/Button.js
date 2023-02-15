bca10ca26fb563dadc51bb89889b78ff87880d69
altamish <altamishpasha@gmail.com>
2023-02-15 12:07:30 +0530

    Button.js updated

--- MERNWebsites/EComerceWebsite/frontend/src/components/Button.js
+++ MERNWebsites/EComerceWebsite/frontend/src/components/Button.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Button({icon,id,color}) {
   return (
     <div className="w-auto h-auto" id={id}>
-        <div className="flex-1 h-full">
+        <div className="flex-1 h-full ">
           <div className={`flex items-center justify-center flex-1 h-full p-2 bg-${color} text-white shadow rounded-full`}>
             <div className="relative">
               {icon}
