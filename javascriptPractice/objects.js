1dad01f41ae14325350cbbd7d366217b3020254d
altamish <altamishpasha@gmail.com>
2023-02-16 11:31:45 +0530

    object practice...

--- javascriptPractice/objects.js
+++ javascriptPractice/objects.js
@@ -15,10 +15,23 @@ Object.defineProperty(obj1, "myname",{
 
 
 
-console.log(obj1.myname);
+// console.log(obj1.myname);
 
 
 const obj2 = {name: "altamish",class: 12};
 
 const proto = Object.getPrototypeOf(obj2);
 // console.log(proto);
+
+
+const obj3 = {
+    name: "altamish",
+    setName: function(){
+        this.name = "altamish weds rikhsar"
+        return this;
+    }
+}
+
+// console.log(obj3);
+// obj3.setName().setName().setName();
+// console.log(obj3);
