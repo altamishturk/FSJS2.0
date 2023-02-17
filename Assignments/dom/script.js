44d7bad72ecd3a8d5ec7fe16038d6d01a4bb0609
altamish <altamishpasha@gmail.com>
2023-02-17 11:32:19 +0530

    completed dom assignment of circle

--- Assignments/dom/script.js
+++ Assignments/dom/script.js
@@ -2,18 +2,22 @@
 document.addEventListener("click",(e)=> {
 
     const div = document.createElement("div");
-    div.style.width = "100px"
-    div.style.height = "100px"
+    div.style.width = "50px"
+    div.style.height = "50px"
+    div.style.top = `${e.y-25}px`;
+    div.style.left = `${e.x-25}px`;
     div.style.background = getRandomColor();
     div.style.position = "absolute";
-    div.style.top = `${e.y-50}px`;
-    div.style.left = `${e.x-50}px`;
     div.style.borderRadius = `50%`;
     div.style.transition = "all 1s";
     document.body.appendChild(div);
     
     setTimeout(() => {
         div.style.opacity = 0;
+        div.style.width = "150px"
+        div.style.height = "150px"
+        div.style.top = `${e.y-75}px`;
+        div.style.left = `${e.x-75}px`;
         setTimeout(()=>{
             div.remove();
         },1000);
