3c8b1318261769b2cbca29c3db23feef1dd871e1
altamish <altamishpasha@gmail.com>
2023-02-21 12:29:19 +0530

    4 assignment of dom completed

--- Assignments/dom/28th Jan JavaScript2.0(DOM Assignment)/DOM Assignments 2.0 Batch copy/DOM Assignment 2.0 1,2,3/about/about.js
+++ Assignments/dom/28th Jan JavaScript2.0(DOM Assignment)/DOM Assignments 2.0 Batch copy/DOM Assignment 2.0 1,2,3/about/about.js
@@ -1,3 +1,6 @@
+secondAssignmenTask1();
+secondAssignmenTask2();
+
 let accordian = document.querySelectorAll(".accordian h3");
 accordian.forEach((element) => {
   element.addEventListener("click", () => {
@@ -9,3 +12,50 @@ accordian.forEach((element) => {
     }
   });
 });
+
+
+function secondAssignmenTask1(){
+      const navigation = document.querySelector("nav ul");
+      const links = navigation.querySelectorAll("li");
+      links[2].remove();
+      
+      ["Projects"].forEach(item => {
+        const li = document.createElement("li");
+        const a = document.createElement("a");
+        a.href = "#";
+        a.innerText = item;
+        li.appendChild(a);
+        navigation.appendChild(li);
+      })
+
+      const accordianWrapper = document.querySelector(".accordian-wrapper");
+      const accordiantitles = accordianWrapper.querySelectorAll("h3");
+      [...accordiantitles].forEach((t,i) => {
+        t.style.backgroundColor = "#FAE6FA";
+        if(i === 2 || i === 3){
+          t.click();
+        }
+      })
+}
+
+
+
+function secondAssignmenTask2(){
+      const accordianWrapper = document.querySelector(".accordian-wrapper");
+      const accordiantitles = accordianWrapper.querySelectorAll("h3");
+      [...accordiantitles].forEach((t,i) => {
+        if(i === 2 || i === 3){
+          t.click();
+        }
+      })
+
+      const accordian = accordianWrapper.querySelector(".accordian");
+      const copyedAccordian = accordian.cloneNode(true);
+      copyedAccordian.querySelector("h3").innerText = "Skills";
+      copyedAccordian.querySelector("p").innerText = "I posses a very good command over the Full Stack Development technologies like MERN which can be seen in my work over the Github";
+      accordianWrapper.appendChild(copyedAccordian);
+      setTimeout(()=> {
+        copyedAccordian.querySelector("h3").click(); 
+      },[1000])
+}
+
